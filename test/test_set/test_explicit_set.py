82a65c06354da7b9736608a769c1e12eae373e82
Sheng-Jung Yu <shengjungyu@gmail.com>
2024-03-05 15:04:08 -0800

    adding fixture

--- test/test_set/test_explicit_set.py
+++ test/test_set/test_explicit_set.py
@@ -1,17 +1,28 @@
 from contractda.sets.explicit_set import ExplicitSet
+import pytest
 
-def test_explicit_set():
-    eset = ExplicitSet(["x", "y", "z"], [(1, 2, 3), (2, 3, 4), (1, 3, 5)])
-    print(eset)
+@pytest.fixture
+def basic_values():
+    return [(1, 2, 3), (2, 3, 4), (1, 3, 5)]
 
-def test_explicit_set_iter():
-    eset = ExplicitSet(["x", "y", "z"], [(1, 2, 3), (2, 3, 4), (1, 3, 5)])
-    for e in eset:
-        print(e)
+@pytest.fixture
+def basic_var_list():
+    return ["x", "y", "z"]
 
-def test_explicit_set_sample():
-    eset = ExplicitSet(["x", "y", "z"], [(1, 2, 3), (2, 3, 4), (1, 3, 5)])
-    print(eset.sample())
+@pytest.fixture
+def basic_explicit_set(basic_var_list, basic_values):
+    eset = ExplicitSet(basic_var_list, basic_values)
+    return eset
+
+def test_explicit_set(basic_explicit_set):
+    print(basic_explicit_set)
+
+def test_explicit_set_iter(basic_explicit_set, basic_values):
+    for e, value in zip(basic_explicit_set, basic_values):
+        assert(e == value)
+
+def test_explicit_set_sample(basic_explicit_set, basic_values):
+    assert(basic_explicit_set.sample() in basic_values)
 
 if __name__ == "__main__":
     test_explicit_set()
